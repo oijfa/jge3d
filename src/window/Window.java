1645e212836cd9394cfdec9debd277f620924744
adam <adam@anewbylt.(none)>
2010-12-02 09:04:25 -0500

    Ok, now it shows a window.

--- src/window/Window.java
+++ src/window/Window.java
@@ -1,33 +1,62 @@
 package window;
 
+import org.lwjgl.input.Keyboard;
+import org.lwjgl.input.Mouse;
+import org.lwjgl.opengl.Display;
+import org.lwjgl.opengl.DisplayMode;
+import org.lwjgl.opengl.GL11;
+
 import window.MainMenu;
 import de.matthiasmann.twl.DesktopArea;
-import de.matthiasmann.twl.FPSCounter;
 import de.matthiasmann.twl.GUI;
-import de.matthiasmann.twl.Label;
+import de.matthiasmann.twl.renderer.lwjgl.LWJGLRenderer;
+import de.matthiasmann.twl.theme.ThemeManager;
 
 
 public class Window extends DesktopArea{
-	private final FPSCounter fpsCounter;
-    private final Label mouseCoords;
     private MainMenu mainMenu;
-    private GUI gui;
+	private boolean quit=false;
 	
 	public static void main(String[] args){
-		Window win = new Window();
+	    try {
+	        Display.setDisplayMode(new DisplayMode(800, 600));
+	        Display.create();
+	        Display.setTitle("JGE3d");
+	        Display.setVSyncEnabled(true);
+	
+	        LWJGLRenderer renderer = new LWJGLRenderer();
+	        Window window = new Window();
+	        GUI gui = new GUI(window, renderer);
+	
+	        ThemeManager theme = ThemeManager.createThemeManager(
+	        	Window.class.getResource("themes/default.xml"), renderer
+	        );
+	        gui.applyTheme(theme);
+	
+	        while(!Display.isCloseRequested() && !window.quit) {
+	            GL11.glClear(GL11.GL_COLOR_BUFFER_BIT);
+	
+	            gui.update();
+	            Display.update();
+	
+	            //Reduce input lag
+	        	GL11.glGetError();          // this call will burn the time between vsyncs
+	        	Display.processMessages();  // process new native messages since Display.update();
+	        	Mouse.poll();               // now update Mouse events
+	        	Keyboard.poll();            // and Keyboard too
+	        }
+	
+	        gui.destroy();
+	        theme.destroy();
+	    } catch (Exception ex) {
+	        ex.printStackTrace();
+	    }
 	}
 	
 	public Window(){
-		super();
-		fpsCounter = new FPSCounter();
-        mouseCoords = new Label();
-
-        add(fpsCounter);
-        add(mouseCoords);
-        
+        //Create the main menu
         mainMenu = new MainMenu();
         add(mainMenu);
-        
         mainMenu.setSize(400, 200);
         mainMenu.setPosition(0, 0);
 	}
