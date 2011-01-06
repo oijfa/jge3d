dac9990360fc93d990b1cbab70d22c6324593d2b
Robb <Mooble.Marble@gmail.com>
2011-01-06 16:23:31 -0500

    Beginnings of mouse input.  Mostly just structure.  Main problem with it right now: Grabs priority from twl so you can't interact with windows

--- src/input/Input.java
+++ src/input/Input.java
@@ -4,6 +4,7 @@ import javax.vecmath.Vector3f;
 
 import org.lwjgl.LWJGLException;
 import org.lwjgl.input.Keyboard;
+import org.lwjgl.input.Mouse;
 
 import entity.Camera;
 import entity.Entity;
@@ -12,6 +13,15 @@ import entity.EntityList;
 public class Input {
 	private Camera camera;
 	private EntityList objectList;
+	private int deltaX;
+	private int deltaY;
+	
+	//ANGLES
+	private static final float LEFT_RIGHT_INC = 0.00001f;
+	private static final float UP_DOWN_INC = 0.00001f;
+	
+	//DISTANCE
+	private static final float IN_OUT_INC = 0.1f;
 	
 	public Input (EntityList objectList){
 		this.objectList = objectList;
@@ -20,7 +30,7 @@ public class Input {
 	public void init(){
 		try {
 			Keyboard.create();
-			//Mouse.create();
+			Mouse.create();
 		} catch (LWJGLException e) {
 			e.printStackTrace();
 		}
@@ -32,45 +42,83 @@ public class Input {
 	public void run(){
 		Keyboard.poll();
 		if(camera != null) {
-			// read keyboard and mouse
-			if(Keyboard.isKeyDown(Keyboard.KEY_RIGHT)){
-				camera.applyCentralImpulse(new Vector3f(-0.001f,0,0));
+			handleMouse();
+			if(Mouse.isInsideWindow()) {
+				handleKeyboard();
 			}
-			if(Keyboard.isKeyDown(Keyboard.KEY_LEFT)){
-				camera.applyCentralImpulse(new Vector3f(0.001f,0,0));
-			}
-			if(Keyboard.isKeyDown(Keyboard.KEY_UP)){
-				camera.applyCentralImpulse(new Vector3f(0,0.001f,0));
-			}
-			if(Keyboard.isKeyDown(Keyboard.KEY_DOWN)){
-				camera.applyCentralImpulse(new Vector3f(0,-0.001f,0));
-			}
-	
-			//TODO: unfuck this scenario what with all the damn 
-			//cam_velocity unknown behavior nonsense
-			//asdf
-			camera.activate();
-			//checks the speed of the camera so it doesn't fly off.
-			Vector3f cam_velocity = new Vector3f();
-			camera.getLinearVelocity(cam_velocity);
-			if(cam_velocity.x > 15 || cam_velocity.x < -15){
-				cam_velocity.x = 0;
-			}
-			if(cam_velocity.y > 15 || cam_velocity.y < -15){
-				cam_velocity.y = 0;
-			}
-			if(cam_velocity.z > 15 || cam_velocity.z < -15){
-				cam_velocity.z = 0;
-			}
-			
-			//assigns the camera a reasonable speed.
-			//(cam_velocity);
-			
-			
-			// Input.getInstance().updateInput();
 		} else {
 			camera = (Camera) objectList.getItem(Camera.CAMERA_NAME);
 		}
 			
+	}
+	
+	private void handleKeyboard(){
+		if(Keyboard.isKeyDown(Keyboard.KEY_RIGHT)){
+			camera.incrementRotation(LEFT_RIGHT_INC);
+		}
+		if(Keyboard.isKeyDown(Keyboard.KEY_LEFT)){
+			camera.incrementRotation(-LEFT_RIGHT_INC);
+		}
+		if(Keyboard.isKeyDown(Keyboard.KEY_UP)){
+			camera.incrementDeclination(UP_DOWN_INC);
+		}
+		if(Keyboard.isKeyDown(Keyboard.KEY_DOWN)){
+			camera.incrementDeclination(-UP_DOWN_INC);
+		}
+	}
+	
+	private void handleMouse(){
+		while(Mouse.next())	{
+			Mouse.poll();
+			
+			//update the changes in position
+			deltaX = Mouse.getEventDX();
+			deltaY = Mouse.getEventDY();
+
+			switch(Mouse.getEventButton()) {
+				case -1://Mouse Movement
+					if(Mouse.isInsideWindow()) {
+						//Editor.getInstance().setCurrentBlock(Mouse.getX(), Mouse.getY(), EditorView.getInstance().getLayer());
+						if(Mouse.isButtonDown(0)) {
+							//Pan camera Z
+							camera.incrementDistance(-1.0f*deltaY);	
+						}
+						
+						if(Mouse.isButtonDown(1)) {
+							//Change angle of camera
+	
+						}
+						
+						if(Mouse.isButtonDown(2)) {
+							//Change Perspective
+							camera.incrementDeclination(-deltaY*.01f);
+							camera.incrementRotation(-deltaX*.01f);
+						}
+					}
+					break;
+				case 0://Left Button
+					if( Mouse.isButtonDown(0) )	{
+					} else {
+					}
+					break;
+				case 1://Right Button
+					if( !(Mouse.isButtonDown(1)) ) {
+					}
+					break;
+				case 2://Middle Button
+					if( !(Mouse.isButtonDown(1)) ) {
+					}
+					break;
+			}
+
+			switch(Mouse.getDWheel()) {
+				case -120: 
+					camera.incrementDistance(IN_OUT_INC);
+					break;
+				case  120: 
+					camera.incrementDistance(-IN_OUT_INC);
+					break;
+			}
+		}
 	}
 }
