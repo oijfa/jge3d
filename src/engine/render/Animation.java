7c386c9ecc10fe9c4385e72edf4d9e77fb1953f4
lampshady <mastacheif@gmail.com>
2011-11-02 17:20:08 -0400

    commited an unsaved file... teehee

--- src/engine/render/Animation.java
+++ src/engine/render/Animation.java
@@ -1,7 +1,12 @@
 package engine.render;
 
+import java.util.HashMap;
+import java.util.LinkedList;
+
 public class Animation {
+	HashMap<String, LinkedList<AnimationFrame>> animation = new HashMap<String,LinkedList<AnimationFrame>>();
+	
 	public Animation() {
-		
+		 
 	}
 }
--- src/engine/render/AnimationFrame.java
+++ src/engine/render/AnimationFrame.java
@@ -4,7 +4,6 @@ public class AnimationFrame {
 	int vboid;
 	int duration;
 	
-	
 	public AnimationFrame(int vboid) {
 		this.vboid = vboid;
 	}
