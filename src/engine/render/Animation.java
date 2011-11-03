b10c73f35b7a015af07eafa403d52550bc24a2d6
lampshady <mastacheif@gmail.com>
2011-11-03 19:19:24 -0400

    Creating file stub for project and updating the animation class

--- src/engine/render/Animation.java
+++ src/engine/render/Animation.java
@@ -1,12 +1,36 @@
 package engine.render;
 
+import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.LinkedList;
 
-public class Animation {
-	HashMap<String, LinkedList<AnimationFrame>> animation = new HashMap<String,LinkedList<AnimationFrame>>();
+public class Animation extends Model {
+	HashMap<String, ArrayList<AnimationFrame>> animation = new HashMap<String,ArrayList<AnimationFrame>>();
+	String last_animation_name;
+	long last_update;
+	int last_frame;
 	
 	public Animation() {
-		 
+		 last_update = System.nanoTime();
+	}
+	
+	public void addFrame(String animation_name, int vboid, int duration) {
+		animation.get(animation_name).add(new AnimationFrame(vboid,duration));
+	}
+	
+	public void nextFrame(String animation_name) {
+		AnimationFrame next_frame = animation.get(animation_name).get(last_frame);
+		if( (last_update+next_frame.getDuration()) <= System.nanoTime()) {
+			last_update = System.nanoTime();
+			if(last_animation_name.equals(animation_name)) {
+				if(last_frame == animation.get(animation_name).size()) {
+					last_frame = 0;
+				} else {
+					last_frame++;
+				}
+			} else {
+				last_frame = 0;
+				last_animation_name = animation_name;
+			}
+		}
 	}
 }
--- src/engine/render/AnimationFrame.java
+++ src/engine/render/AnimationFrame.java
@@ -1,10 +1,15 @@
 package engine.render;
 
 public class AnimationFrame {
-	int vboid;
-	int duration;
+	private int vboid;
+	private int duration;
 	
-	public AnimationFrame(int vboid) {
+	public AnimationFrame(int vboid, int duration) {
 		this.vboid = vboid;
+		this.duration = duration;
+	}
+	
+	public int getDuration() {
+		return duration;
 	}
 }
