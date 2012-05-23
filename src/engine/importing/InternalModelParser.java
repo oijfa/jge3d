f044a093057f0052275a20c70b33a66c8edab3d0
Robbie Nolan <mooble.marble@gmail.com>
2012-05-22 20:58:26 -0400

    internal model format laid out

--- src/engine/importing/InternalModelParser.java
+++ src/engine/importing/InternalModelParser.java
@@ -1,5 +1,110 @@
 package engine.importing;
 
-public class InternalModelParser {
+import java.io.IOException;
+import java.io.DataInputStream;
+import java.net.URL;
 
+import engine.render.Model;
+
+public class InternalModelParser {
+	private static final int SIZE_TRANSLATION = 12;
+	private static final int SIZE_ROTATION = 8;
+	private static final int SIZE_NUM_PRIMS = 2;
+	private static final int SIZE_BLOCK = 28;
+	private static final int SIZE_FACE = 60;
+	private static final int SIZE_NUM_MATS = 2;
+	private static final int SIZE_MAT = 52;
+	private static final int SIZE_NUM_MESHES = 2;
+	private static final int SIZE_NUM_REFS = 2;
+	
+
+	DataInputStream input;
+	
+	boolean use_blocks;
+	int number_of_primatives;
+	int number_of_materials;
+	int number_of_meshes;
+	int number_of_objects;
+	
+	
+	Model readFile(URL url) throws Exception{
+		try{
+			byte[] bytes;
+			
+			input = new DataInputStream(url.openStream());
+			
+			use_blocks = (input.readByte()==1);
+	
+			bytes = new byte[SIZE_NUM_PRIMS];
+			number_of_primatives = byteArrayToInt(bytes);
+			
+			for(int i = 0; i < number_of_primatives; i++){
+				if(use_blocks){
+					bytes = new byte[SIZE_BLOCK];
+					readBlock(bytes);
+				}else{
+					bytes = new byte[SIZE_FACE];
+					readFace(bytes);
+				}
+			}
+			
+			bytes = new byte[SIZE_NUM_MATS];
+			number_of_materials = byteArrayToInt(bytes);
+			for(int i = 0; i < number_of_materials; i++){
+				bytes = new byte[SIZE_MAT];
+				readMaterial(bytes);
+			}
+			
+			bytes = new byte[SIZE_NUM_MESHES];
+			number_of_meshes = byteArrayToInt(bytes);
+			for(int i = 0; i < number_of_meshes; i++){
+				boolean mesh_refs = (input.readByte()==1);
+				bytes = new byte[SIZE_NUM_REFS];
+				int num_refs = byteArrayToInt(bytes);
+				
+				bytes = new byte[(num_refs*SIZE_NUM_PRIMS) + SIZE_TRANSLATION + SIZE_ROTATION];
+				input.read(bytes);
+				
+				readMesh(bytes,mesh_refs);
+			}
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	private void readMesh(byte[] bytes, boolean mesh_refs) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void readMaterial(byte[] bytes) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void readFace(byte[] bytes) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void readBlock(byte[] bytes) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	byte[] readBytes(int num) throws IOException{
+		byte[] bytes = new byte[num];
+		input.read(bytes);
+		return bytes;
+	}
+	
+	public static int byteArrayToInt(byte[] b) {
+	    int value = 0;
+	    for (int i = 0; i < 4; i++) {
+	        int shift = (4 - 1 - i) * 8;
+	        value += (b[i] & 0x000000FF) << shift;
+	    }
+	    return value;
+	}
 }
